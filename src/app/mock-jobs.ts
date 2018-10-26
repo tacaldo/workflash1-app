4188ee100b8e2150c5906cec2ac52b05285862b9
Tony Acaldo <tonyacaldo@charter.net>
2018-10-26 19:18:41 -0400

    Latest

--- src/app/mock-jobs.ts
+++ src/app/mock-jobs.ts
@@ -1,14 +1,12 @@
 import { Job } from './job';
 
 export const JOBS: Job[] = [
-  { id: 11, name: 'Mr. Nice.' },
-  { id: 12, name: 'Narcoccc' },
-  { id: 13, name: 'Bombastoccc' },
-  { id: 14, name: 'Celeritasccc' },
-  { id: 15, name: 'Magnetaccc' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Construction Day Labor' },
+  { id: 12, name: 'Warehouse Worker' },
+  { id: 13, name: 'Furniture Mover' },
+  { id: 14, name: 'Load Trucks' },
+  { id: 15, name: 'Cleanup Crew - Festival' },
+  { id: 16, name: 'Welder' },
+  { id: 17, name: 'Mechanic Assistant' },
+  { id: 18, name: 'Roofing Helper' }
 ];
