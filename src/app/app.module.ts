3cf1bcca385e58d2b454f76218725dadba6de0b1
Tony Acaldo <tonyacaldo@charter.net>
2018-10-27 12:10:06 -0400

    Add messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { JobDetailComponent } from './job-detail/job-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JobsComponent,
-    JobDetailComponent
+    JobDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
