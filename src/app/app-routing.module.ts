2d27c298ffb0cd316ec04cd877a830d55e3e630f
Tony Acaldo <tonyacaldo@charter.net>
2018-10-27 22:52:25 -0400

    Latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JobsComponent }      from './jobs/jobs.component';
 import { DashboardComponent }   from 'src/app/components/dashboard/dashboard.component';
+import { JobDetailComponent }  from './job-detail/job-detail.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: JobDetailComponent }, 
   { path: 'jobs', component: JobsComponent }
 ];
 
