dc65814ae4642b52b56130e1cf0e36d9fe6bce7c
Tony Acaldo <tonyacaldo@charter.net>
2018-10-29 17:35:36 -0400

    Add in memory data services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,27 @@ import { JobDetailComponent } from './job-detail/job-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
+
   ],
   declarations: [
     AppComponent,
@@ -23,5 +38,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent
   ],
   bootstrap: [AppComponent]
+
+
+
+
 })
 export class AppModule { }
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -3,6 +3,8 @@ import { Job } from '../job';
 import { JOBS } from '../mock-jobs';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,20 +12,60 @@ import { MessageService } from './message.service';
 })
 export class JobService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+    private jobsUrl = 'api/jobs';  // URL to web api
 
 
-    getJobs(): Observable<Job[]> {
-    // TODO: send the message _after_ fetching the heroes
-      this.messageService.add('JobService: fetched jobs');
-      return of(JOBS);
+    // getJobs(): Observable<Job[]> {
+    // // TODO: send the message _after_ fetching the heroes
+    //   this.messageService.add('JobService: fetched jobs');
+    //   return of(JOBS);
+    // }
+
+  /** GET heroes from the server */
+  getJobs (): Observable<Job[]> {
+    return this.http.get<Job[]>(this.jobsUrl)
+      .pipe(
+        catchError(this.handleError('getJobs', []))
+      );
+  }
+
+  /** GET job by id. Will 404 if id not found */
+  getJob(id: number): Observable<Job> {
+    const url = `${this.jobsUrl}/${id}`;
+    return this.http.get<Job>(url).pipe(
+      tap(_ => this.log(`fetched job id=${id}`)),
+      catchError(this.handleError<Job>(`getHero id=${id}`))
+    );
+  }
+
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+      this.messageService.add(`JobService: ${message}`);
     }
 
-    getJob(id: number): Observable<Job> {
-      // TODO: send the message _after_ fetching the hero
-      this.messageService.add(`HeroService: fetched hero id=${id}`);
-      return of(JOBS.find(job => job.id === id));
-    }
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
 
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 
   }
