eb60cd08a54b386a4950cc68ad5c03c7159e95d5
Tony Acaldo <tonyacaldo@charter.net>
2018-10-30 00:20:27 -0400

    Add search behavior

--- src/app/components/job-search/job-search.component.ts
+++ src/app/components/job-search/job-search.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+import { Job } from '../../job';
+import { JobService } from '../../services/job.service';
+
 @Component({
   selector: 'app-job-search',
   templateUrl: './job-search.component.html',
-  styleUrls: ['./job-search.component.css']
+  styleUrls: [ './job-search.component.css' ]
 })
 export class JobSearchComponent implements OnInit {
+  jobs$: Observable<Job[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private jobService: JobService) {}
 
-  ngOnInit() {
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.jobs$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.jobService.searchJobs(term)),
+    );
+  }
 }
